5d4eac114eb9f42f26103500684c492556b1b2ff
MACK Mathieu <mathieu.mack@econocom.com>
2017-12-05 21:29:06 +0100

    Fix script package

--- nuspec/packageGenerator.ps1
+++ nuspec/packageGenerator.ps1
@@ -14,7 +14,7 @@ write-host "Update the nuget.exe file" -foreground "DarkGray"
 
 $apiKey = $env:NuGetApiKey
 
-$strPath = $location + '\ValidationUtils\ValidationUtils\bin\Release\netstandard1.4\ValidationUtils.dll'
+$strPath = $location + '\ValidationUtils\bin\Release\netstandard1.4\ValidationUtils.dll'
 
 $VersionInfos = [System.Diagnostics.FileVersionInfo]::GetVersionInfo($strPath)
 $ProductVersion = $VersionInfos.ProductVersion
@@ -22,6 +22,6 @@ write-host "Product version : " $ProductVersion -foreground "Green"
 	
 write-host "Publish nuget packages" -foreground "Green"
 
-$packagePath = "../ValidationUtils/ValidationUtils/bin/Release/ValidationUtils.$ProductVersion.nupkg"
+$packagePath = "../ValidationUtils/bin/Release/ValidationUtils.$ProductVersion.nupkg"
 write-host $packagePath -foreground "DarkGray"
 .\NuGet push $packagePath -Source https://www.nuget.org/api/v2/package -ApiKey $apiKey
